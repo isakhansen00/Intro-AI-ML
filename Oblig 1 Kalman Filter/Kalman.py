7ad757542ca1da6295de1b27ce6342f344c1fc59
Isak Steinmo Hansen <isakhanseen00@gmail.com>
2022-10-20 11:39:36 +0200

    .

--- Oblig 1 Kalman Filter/Kalman.py
+++ Oblig 1 Kalman Filter/Kalman.py
@@ -1,26 +1,22 @@
 class Kalman():
     def __init__(self):
         """ Initialize filter """
-        self.x_current = 108
-        self.v_current = 0.7
+        self.x_current = 0
+        self.v_current = 0.3
         self.dt = 1
-        self.a_current = 0.0015
-        self.x_pred = self.x_current
-        self.v_pred = self.v_current
-        self.a_pred = self.a_current
-
-        #self.alpha = 0.000001
-        #self.beta = 0.000052
-        #self.gamma = 0.0000015
-        self.alpha = 0.01
+        self.a_current = 0.003
+        self.alpha = 0.5
         self.beta = 0.0001
         self.gamma = 0.00001
+
+        self.x_pred = self.x_current + self.v_current*self.dt + self.a_current*((self.dt**2)*0.5)
+        self.v_pred = self.v_current + self.a_current*self.dt
+        self.a_pred = self.a_current
 
         
         
     def estimate_current_position_and_velocity(self, zi):
         """ Estimate current position and velocity from (noisy) position measurement """ 
-        
 
         # State update equation
         self.x_current = self.x_pred + self.alpha*(zi - self.x_pred)
@@ -30,8 +26,6 @@ class Kalman():
         # State extrapolation equation
         self.x_pred = self.x_current + self.v_current*self.dt + self.a_current*((self.dt**2)*0.5)
         self.v_pred = self.v_current + self.a_current*self.dt
-        #self.a_pred = self.a_current
+        self.a_pred = self.a_current
 
-
-
-        return self.x_pred, self.v_pred
+        return self.x_current, self.v_current
